f1186de3b313e2b89253975a5c3fb50277452e4f
PedroTrudes <pedro.jtrudes@gmail.com>
2025-07-21 22:20:39 -0300

    :sparkles: feat: authentication done

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import Login from './screens/Login';
 import Register from './screens/Register';
+import './reset.scss';
+import Feed from './screens/Feed';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Login /> 
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Login />} />
+      <Route path='/feed' element={<Feed />} />
+    </Routes>
+  </BrowserRouter>
 )
+/*
+<StrictMode>
+    <Login /> 
+</StrictMode>,
+*/
--- src/screens/Login/index.jsx
+++ src/screens/Login/index.jsx
@@ -1,30 +1,71 @@
 import AssetLogin from '../../assets/SamsungDevices.jpg';
 import './login.scss'
 import React, {useState} from 'react'
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 function Login() {
+    const navigate = useNavigate();
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
+    const [token, setToken] = useState(null);
+    const [errorRequest, setErrorRequest] = useState('');
     const [errorPassword, setErrorPassword] = useState(true);
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        try {
+            const res = await axios.post('http://localhost:5000/mycheil/login', {
+               login_user : login,
+                password_user: password
+            });
+
+            const saveToken = res.data.token;
+            setToken(saveToken);
+            localStorage.setItem('token', saveToken);
+            setMessage('Logado com sucesso')
+
+            navigate('/feed');
+            
+        } catch (error) {
+            console.error('Erro ao authenticar', error);
+            setMessage('Email ou senha invalidos');
+        }
+    }
     
     return(
         <div className="containerLogin">
             <div className="containerImage">
+                
                 <img src={AssetLogin} alt="Imagens animado do mycheil docs" />
             </div>
             <div className="containerForm">
                 <div className="formLogo">
-                    <img src="" alt="Logo do myCheilDocs" />
                 </div>
                 <div className="form">
-                    <form method="get" className="formLogin">
+                    <form method="get" className="formLogin" onSubmit={handleLogin}>
                         <label htmlFor='email'>Usuario</label>
-                        <input type="text" name="email" placeholder="email@cheil.com"/>
+                        <input type="text" name="email" placeholder="email@cheil.com"
+                        value={login}
+                        onChange={(e) => setLogin(e.target.value)}
+                        />
                         <label htmlFor="senha">Senha</label>
-                        <input type="text" name="senha" placeholder="Password"/>    
+                        <input type="text" name="senha" placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        />    
                         <span>*Esqueceu a senha?</span>
                         
                         <div className="buttonForm">
                             <button>Logar</button>    
                         </div>    
+
+                        { message && (
+                                <p>{message}</p>
+                            )
+                        }
                     </form>
                 </div>
             </div>
@@ -32,5 +73,7 @@ function Login() {
         
     )
 }
+
+// <img src="" alt="Logo do myCheilDocs" />
 
 export default Login;
