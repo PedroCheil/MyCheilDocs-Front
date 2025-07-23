6eb4e7e42d1f4e61c979e337e1085bce1e5c991f
PedroTrudes <pedro.jtrudes@gmail.com>
2025-07-23 20:48:00 -0300

    :sparkles: feat: new component alert in project

--- src/screens/Login/index.jsx
+++ src/screens/Login/index.jsx
@@ -1,77 +1,102 @@
-import AssetLogin from '../../assets/SamsungDevices.jpg';
-import './login.scss'
-import React, {useState} from 'react'
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import AssetLogin from "../../assets/SamsungDevices.jpg";
+import "./login.scss";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import AlertMessage from "../../components/alertMessage";
 
 function Login() {
-    const navigate = useNavigate();
-    const [login, setLogin] = useState('');
-    const [password, setPassword] = useState('');
-    const [message, setMessage] = useState('');
-    const [token, setToken] = useState(null);
-    const [errorRequest, setErrorRequest] = useState('');
-    const [errorPassword, setErrorPassword] = useState(true);
+  const navigate = useNavigate();
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+  const [token, setToken] = useState(null);
+  const [alert, setAlert] = useState(null);
+  const [sucessLogin, setSucessLogin] = useState('wait');
 
-    const handleLogin = async (e) => {
-        e.preventDefault();
-
-        try {
-            const res = await axios.post('http://localhost:5000/mycheil/login', {
-               login_user : login,
-                password_user: password
-            });
-
-            const saveToken = res.data.token;
-            setToken(saveToken);
-            localStorage.setItem('token', saveToken);
-            setMessage('Logado com sucesso')
-
-            navigate('/feed');
-            
-        } catch (error) {
-            console.error('Erro ao authenticar', error);
-            setMessage('Email ou senha invalidos');
-        }
-    }
+  const alertMessage = () => {
+    if (sucessLogin == "error") {
+      setAlert({
+        typeAlert: "error",
+        messageAlert: "Usuario ou senha invalido",
+      });
+    } 
     
-    return(
-        <div className="containerLogin">
-            <div className="containerImage">
-                
-                <img src={AssetLogin} alt="Imagens animado do mycheil docs" />
-            </div>
-            <div className="containerForm">
-                <div className="formLogo">
-                </div>
-                <div className="form">
-                    <form method="get" className="formLogin" onSubmit={handleLogin}>
-                        <label htmlFor='email'>Usuario</label>
-                        <input type="text" name="email" placeholder="email@cheil.com"
-                        value={login}
-                        onChange={(e) => setLogin(e.target.value)}
-                        />
-                        <label htmlFor="senha">Senha</label>
-                        <input type="text" name="senha" placeholder="Password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        />    
-                        <span>*Esqueceu a senha?</span>
-                        
-                        <div className="buttonForm">
-                            <button>Logar</button>    
-                        </div>    
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
 
-                        { message && (
-                                <p>{message}</p>
-                            )
-                        }
-                    </form>
-                </div>
+  useEffect(() => {
+    alertMessage();
+  }, [sucessLogin]);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+        const res = await axios.post("http://localhost:5000/mycheil/login", {
+            login_user: login,
+            password_user: password,
+        });
+
+        const saveToken = res.data.token;
+        setToken(saveToken);
+        localStorage.setItem("token", token);
+        navigate("/feed");
+    } catch (error) {
+        alertMessage();
+        setSucessLogin('error');
+        setLogin("");
+        setPassword("");
+    }
+  };
+
+  return (
+    <div className="containerLogin">
+      <div className="containerImage">
+        <img src={AssetLogin} alt="Imagens animado do mycheil docs" />
+      </div>
+      <div className="containerForm">
+        <div className="formLogo"></div>
+        <div className="form">
+          <form method="post" className="formLogin" onSubmit={handleLogin}>
+            <label htmlFor="email">Usuario</label>
+            <input
+              type="text"
+              name="email"
+              placeholder="email@cheil.com"
+              value={login}
+              onChange={(e) => setLogin(e.target.value)}
+              required
+            />
+            <label htmlFor="senha">Senha</label>
+            <input
+              type="text"
+              name="senha"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <span>*Esqueceu a senha?</span>
+
+            <div className="buttonForm">
+              <button>Logar</button>
             </div>
+            <div className="alertForm">
+              {alert && (
+                <AlertMessage
+                  messageAlert={alert.messageAlert}
+                  typeAlert={alert.typeAlert}
+                  style={{ fontWeight: "bold" }}
+                />
+              )}
+            </div>
+          </form>
         </div>
-        
-    )
+      </div>
+    </div>
+  );
 }
 
 // <img src="" alt="Logo do myCheilDocs" />
