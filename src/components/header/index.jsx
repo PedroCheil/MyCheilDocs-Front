b604c5ccb106003daa564728f940137e0c005b61
PedroTrudes <pedro.jtrudes@gmail.com>
2025-10-18 01:23:52 -0300

    :construction: header application

---
+++ src/components/header/index.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./navbar";
+function HeaderApplication() {
+  return (
+    <div>
+      <Navbar />
+      <h2>MyCheil-DOCS</h2>
+    </div>
+  );
+}
+
+export default HeaderApplication;
---
+++ src/components/header/userIcon/index.jsx
@@ -0,0 +1,9 @@
+function UserIcon() {
+    return(
+        <>
+            <span>Chamando icone de usuario</span>
+        </>
+    )
+}
+
+export default UserIcon;
